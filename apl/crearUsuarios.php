0b0f47ea9c8017930575d4407fdedd34a06e4a19
diMorillas <didacmorillas19@gmail.com>
2025-01-13 17:11:32 +0100

    Solved crearUsuario function

--- apl/crearUsuarios.php
+++ apl/crearUsuarios.php
@@ -71,7 +71,7 @@
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-        if(isset($_POST['crearGestor'] == "1")){
+        if (isset($_POST['crearGestor']) && $_POST['crearGestor'] == "1") {
             $nombreUsuario = $_POST['usuarioGestor'];
             $nombre = $_POST['nombreGestor'];
             $idNumerico = $_POST['idGestor'];
@@ -79,13 +79,12 @@
             $email = $_POST['emailGestor'];
             $telContacto = $_POST['telContactoGestor'];
             //$contraseña = hash('sha256', $contraseña); // Encriptar contraseña
-            $email = $_POST['emailGestor'];
-
+    
             evitarRepetidos($idNumerico, __DIR__ . "/usuarios.txt");
-            crearUsuario($nombreUsuario,$idNumerico,$contraseña,$nombre,$email,$telContacto,"null",__DIR__ . "/usuarios.txt","gestor");
-
+            crearUsuario($nombreUsuario, $idNumerico, $contraseña, $nombre, $email, $telContacto, "null", __DIR__ . "/usuarios.txt", "gestor");
         }
     }
+    
         
 
     ?>
