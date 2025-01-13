25a2df4b9754ee574f35c51372353ce2e03b302f
rekloyd <paumorillasrivera129@gmail.com>
2025-01-13 17:45:40 +0100

    still debugging creation

--- apl/crearUsuarios.php
+++ apl/crearUsuarios.php
@@ -19,18 +19,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         //CRUD DEL GESTOR
     
         if ($_POST['crearGestor'] == "1") {
-            $nombreUsuario = $_POST['usuarioGestor'];
-            $nombre = $_POST['nombreGestor'];
-            $idNumerico = $_POST['idGestor'];
-            $contraseña = $_POST['contraseñaGestor'];
-            $email = $_POST['emailGestor'];
-            $telContacto = $_POST['telContactoGestor'];
-    
-            if (evitarRepetidos($idNumerico, __DIR__ . "/usuarios.txt")) {
-               header('Location: admin.php?creado=repetido')
-            } else {
-                crearUsuario($nombreUsuario, $idNumerico, $contraseña, $nombre, $email, $telContacto, "null", __DIR__ . "/usuarios.txt", "gestor");
-            }elseif ($_POST['modificarGestor'] == "1") {
                 $nombreUsuario = $_POST['usuarioGestor'];
                 $nombre = $_POST['nombreGestor'];
                 $idNumerico = $_POST['idGestor'];
@@ -39,28 +27,37 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $telContacto = $_POST['telContactoGestor'];
         
                 if (evitarRepetidos($idNumerico, __DIR__ . "/usuarios.txt")) {
-                   header('Location: admin.php?creado=repetido')
-                } else {
-                    modificarUsuario($nombreUsuario, $idNumerico, $contraseña, $nombre, $email, $telContacto, "null", __DIR__ . "/usuarios.txt", "gestor");
+                    header('Location: admin.php?creado=repetido');
                 }
-    
-        //CRUD DEL CLIENTE
-    
-        }elseif ($_POST['crearCliente'] == "1"){
-            $nombreUsuario = $_POST['usuarioCliente'];
-            $nombre = $_POST['nombreCliente'];
-            $idNumerico = $_POST['idCliente'];
-            $contraseña = $_POST['contraseñaCliente'];
-            $email = $_POST['emailCliente'];
-            $telContacto = $_POST['telContactoCliente'];
-            $codigoPostal = $_POST['codigoPostalCliente'];
-            crearUsuario($nombreUsuario, $idNumerico, $contraseña, $nombre, $email, $telContacto, $codigoPostal, __DIR__ . "/usuarios.txt", "cliente");
-        }
-    
-    
-        //CRUD DEL ADMIN
-    }
+                } elseif(crearUsuario($nombreUsuario, $idNumerico, $contraseña, $nombre, $email, $telContacto, "null", __DIR__ . "/usuarios.txt", "gestor");
+                    header('Location: admin.php?creado=exito');
+                ) {
+                }elseif ($_POST['modificarGestor'] == "1") {
+                    $nombreUsuario = $_POST['usuarioGestor'];
+                    $nombre = $_POST['nombreGestor'];
+                    $idNumerico = $_POST['idGestor'];
+                    $contraseña = $_POST['contraseñaGestor'];
+                    $email = $_POST['emailGestor'];
+                    $telContacto = $_POST['telContactoGestor'];
+            
+                    if (evitarRepetidos($idNumerico, __DIR__ . "/usuarios.txt")) {
+                    header('Location: admin.php?creado=repetido')
+                    } else {
+                        modificarUsuario($nombreUsuario, $idNumerico, $contraseña, $nombre, $email, $telContacto, "null", __DIR__ . "/usuarios.txt", "gestor");
+                    }
+        
+            }elseif ($_POST['crearCliente'] == "1"){
+                $nombreUsuario = $_POST['usuarioCliente'];
+                $nombre = $_POST['nombreCliente'];
+                $idNumerico = $_POST['idCliente'];
+                $contraseña = $_POST['contraseñaCliente'];
+                $email = $_POST['emailCliente'];
+                $telContacto = $_POST['telContactoCliente'];
+                $codigoPostal = $_POST['codigoPostalCliente'];
+                crearUsuario($nombreUsuario, $idNumerico, $contraseña, $nombre, $email, $telContacto, $codigoPostal, __DIR__ . "/usuarios.txt", "cliente");
+                }
 }
+
     
     ?>
 </body>
