4d2b49344cbe3f8db2a970b9c18223e619ce4cb0
diMorillas <didacmorillas19@gmail.com>
2025-01-15 16:26:56 +0100

    HTTPS page

--- apl/index.php
+++ apl/index.php
@@ -104,24 +104,25 @@ $cart_empty = !(isset($_SESSION['cart']) && count($_SESSION['cart']) > 0);
         <h1>AliMorillas</h1>
     </div>
     <nav>
-        <ul class="nav-links">
-            <li><a href="index.php">Inicio</a></li>
-            <li><a href="ayuda.php">Ayuda</a></li>
-            <?php
-            if ($usuario) {
-                echo "<li style='color:blue; font-weight:bold;'><a href='areasPersonales.php?tipo=" . htmlspecialchars($tipoUsuario) . "' style='color:inherit;'>Hola, " . strtoupper(htmlspecialchars($usuario)) . "</a></li>";
-                echo "<li><a href='carrito.php' style='display: inline-flex; align-items: center;'>
-            <svg xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 -960 960 960' width='24px' fill='#e8eaed'>
-                <path d='M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z'/>
-            </svg>
-        </a></li>";
-                echo "<li><a href='logout.php' class='log-in'>Cerrar sesión</a></li>";
-            } else {
-                echo "<li><a href='login.html' class='log-in'>Log In</a></li>";
-            }
-            ?>
-        </ul>
-    </nav>
+    <ul class="nav-links">
+        <li><a href="index.php">Inicio</a></li>
+        <li><a href="ayuda.php">Ayuda</a></li>
+        <?php
+        if ($usuario) {
+            echo "<li class='user-link'><a href='areasPersonales.php?tipo=" . htmlspecialchars($tipoUsuario) . "'>Hola, " . strtoupper(htmlspecialchars($usuario)) . "</a></li>";
+            echo "<li><a href='carrito.php' class='cart-link'>
+                <svg xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 -960 960 960' width='24px' fill='#e8eaed'>
+                    <path d='M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z'/>
+                </svg>
+            </a></li>";
+            echo "<li><a href='logout.php' class='log-in'>Cerrar sesión</a></li>";
+        } else {
+            echo "<li><a href='login.html' class='log-in'>Log In</a></li>";
+        }
+        ?>
+    </ul>
+</nav>
+
 </header>
 <img src="/phpEcomProject/imatges/banner-ali.webp" height="720px" width="100%">
 <main>
