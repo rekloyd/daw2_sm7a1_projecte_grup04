69fb1c43a732a21486db1ab95065eb16da0ad769
diMorillas <didacmorillas19@gmail.com>
2025-01-15 17:34:04 +0100

    Now products are correctly added to cart

--- apl/añadirCarrito.php
+++ apl/añadirCarrito.php
@@ -2,20 +2,30 @@
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $nombreProducto = $_POST['name'];
-    $idProducto = $_POST['id'];
-    $precioProducto = $_POST['price'];
+    $idProducto = $_POST['product_id'];
+    $nombreProducto = $_POST['product_name'];
+    $precioProducto = $_POST['product_price'];
+    $cantidad = $_POST['quantity'];
 
+    // Inicializar el carrito si no existe
     if (!isset($_SESSION['cart'])) {
         $_SESSION['cart'] = [];
     }
 
-    $_SESSION['cart'][] = [
-        'nombre' => $nombreProducto,
-        'id' => $idProducto,
-        'precio' => $precioProducto
-    ];
+    // Verificar si el producto ya está en el carrito
+    if (isset($_SESSION['cart'][$idProducto])) {
+        // Incrementar la cantidad
+        $_SESSION['cart'][$idProducto]['quantity'] += $cantidad;
+    } else {
+        // Añadir el producto al carrito
+        $_SESSION['cart'][$idProducto] = [
+            'name' => $nombreProducto,
+            'price' => $precioProducto,
+            'quantity' => $cantidad
+        ];
+    }
 
+    // Redirigir al usuario de vuelta a la página principal
     header("Location: index.php");
     exit();
 }
