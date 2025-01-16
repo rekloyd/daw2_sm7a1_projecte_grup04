34756c6f4e11c2c0434991554f02309613bf1900
rekloyd <paumorillasrivera129@gmail.com>
2025-01-16 17:09:16 +0100

    updated files to be the same to htdocs

--- apl/index.php
+++ apl/index.php
@@ -1,10 +1,20 @@
-<?php
-session_start();
+<!DOCTYPE html>
+<html lang="en">
 
-// Verificar si existe la clave 'username' en la sesión
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AliMorillas</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+</head>
+
+<?php
+
 $usuario = isset($_SESSION['username']) ? $_SESSION['username'] : NULL;
-$tipoUsuario = isset($_SESSION['tipo']) ? $_SESSION['tipo'] : NULL;
-// Ruta al archivo de texto
+$tipoUsuario = isset($_SESSION['tipoUsuario']) ? $_SESSION['tipoUsuario'] : NULL;
 $archivo = __DIR__.'/../productos.txt';
 
 // Verificar si el archivo existe
@@ -73,14 +83,13 @@ if (file_exists($archivo)) {
 <body>
 <header>
 
-        <nav>
+<nav>
             <ul class="nav-links">
-                <img src="../imatges/favicon.png" alt="logo" width="80px">
                 <li><a href="index.php">Inicio</a></li>
                 <li><a href="ayuda.php">Ayuda</a></li>
                 <?php
                 if ($usuario) {
-                    echo "<li style='color:blue; font-weight:bold;'><a href='areasPersonales.php?tipo=" . htmlspecialchars($tipoUsuario) . "' style='color:inherit;'>Hola, " . strtoupper(htmlspecialchars($usuario)) . "</a></li>";
+                    echo "<li style='color:blue; font-weight:bold;'><a href='areasPersonales.php?tipoUsuario=" . htmlspecialchars($tipoUsuario) . "' style='color:inherit;'>Hola, " . strtoupper(htmlspecialchars($usuario)) . "</a></li>";
                     echo "<li><a href='carrito.php' style='display: inline-flex; align-items: center;'>
                 <svg xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 -960 960 960' width='24px' fill='#e8eaed'>
                     <path d='M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z'/>
